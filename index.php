0ac3358071272e215b4d0ac57cf5a750dc29bc5f
projectzenic <50314030+projectzenic@users.noreply.github.com>
2020-09-24 15:29:38 -0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hello world!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis vel soluta deleniti impedit voluptates repellat aliquid provident fugiat harum debitis. Error eveniet inventore saepe quod iusto alias suscipit dolorem. Modi.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
